d23ae91f033df2ef8c5be9392896863fb4b445b8
Stephen491 <60985179+Stephen491@users.noreply.github.com>
2020-04-13 02:20:50 -0400

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -9,6 +9,5 @@ def getCost(x, y, theta, lambda1):
 def gradient(x,y ,theta, lambda1):
     m = np.size(x,0)
     h_0 = np.dot(x,theta)
-    #h_0 = x*theta
     grad = (1/m)*(np.sum(((h_0-y)*x),0)).reshape(np.size(theta,0),1) + (lambda1/m)*np.sum(np.vstack([[0],theta[1:]]))
     return grad
